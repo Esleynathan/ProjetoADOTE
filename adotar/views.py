72dd24f2be7d36f1a9387e3873229ea20cfb9e7e
Esley <esleynathan@hotmail.com>
2023-01-14 18:38:30 -0300

    Projeto ADO.TE - Solicitar adoção

--- adotar/views.py
+++ adotar/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from divulgar.models import Pet, Raca
+from django.contrib.messages import constants
+from django.contrib import messages
+from .models import PedidoAdocao
+from datetime import datetime
 
 def listar_pets (request):
     if request.method == "GET":
@@ -17,3 +21,20 @@ def listar_pets (request):
             raca_filter = Raca.objects.get(id=raca_filter)
 
         return render(request, 'listar_pets.html',{'pets': pets, 'racas': racas, 'cidade': cidade, 'raca_filter': raca_filter })
+
+def pedido_adocao(request, id_pet):
+    pet = Pet.objects.filter(id=id_pet).filter(status="P")
+    
+    if not pet.exists():
+        messages.add_message(request, constants.WARNING, 'Esse pet já foi adotado')
+        return redirect('/adotar')
+    
+    pedido = PedidoAdocao(pet=pet.first(),
+                        usuario=request.user,
+                        data=datetime.now())
+
+    pedido.save()
+    
+    messages.add_message(request, constants.SUCCESS, 'Pedido de adoção realizado com sucesso.')
+
+    return redirect('/adotar')
