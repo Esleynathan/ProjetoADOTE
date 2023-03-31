09951bcaa314b98b4cb4fa6d80afa32834e29860
Ésley Nathan <31036923+Esleynathan@users.noreply.github.com>
2023-03-31 00:42:50 -0300

    Resolvendo erro v2

--- adote/settings.py
+++ adote/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'adote.urls'
@@ -127,11 +126,6 @@ STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join('static')    
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
